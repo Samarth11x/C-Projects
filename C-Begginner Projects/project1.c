143bc1e06f3f7135a8231246565b77246f6efed9
Samarth11x <samarthpatil3996@gmail.com>
2024-12-28 08:53:05 +0530

    Made Changes in comments

--- C-Begginner Projects/project1.c
+++ C-Begginner Projects/project1.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
-#include<stdlib.h>  // to generate random number;
-#include<time.h>    // to seed random number using current time;
+#include<stdlib.h>  // to generate random number
+#include<time.h>    // to seed random number using current time
 
 int main(){
     int random, guess;
-    int no_of_attempts;
+    int no_of_attempts = 0;      // Initialize the number of attempts
 
     srand(time(NULL)); 
-     // srand = seed random number ;
-    //  (time(NUll)) = using current time (NULL) = asign null in time lib ;
+     // srand = seed random number 
+    //  (time(NUll)) = using current time to seed the random number generator
 
 
     printf("\n \t * Welcome to NUMBER GUESSING game *\n");
 
     random = (rand() % 20 + 1 );
-    // rand() % 20  will give values from 1 - 19 (+1) to do 1 - 20 ;
+    // rand() % 20  will give values from 1 - 19 (+1) to do 1 - 20 
 
 
     do{
         printf("\nEnter a Number between (1 - 20) : ");
         scanf("%d", &guess);
-        no_of_attempts++; // to add the attempt every time user enters a number ;
+        no_of_attempts++;     // increment the attempt count each time the user enters a number 
 
         if(random > guess){
             printf("Guess a greater number.\n");
@@ -29,7 +29,7 @@ int main(){
             printf("Guess a  smaller number.\n");
         }
         else{
-            printf("\n \t Congratulation!! \n You have guessed a right Number in %d attempt\n", no_of_attempts );
+            printf("\n \t Congratulations !! \n You have guessed a right Number in %d attempt\n", no_of_attempts );
         }
 
     }while(guess != random);
@@ -39,5 +39,5 @@ int main(){
     printf("\nDeveloped by : SAM \n");
 
 
-
+    return 0;
 }
